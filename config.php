d226312f887b7d27320d163dc6359935675431b9
jonard <gaetan.jonard@outlook.fr>
2020-03-06 11:06:52 +0100

    rectif

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 session_start();
 
 //include_once 'controllers/configCtrl.php';
-$admin=array(255555);
+$admin=array(25689);
 $authAdmin = array(25689, 2565);
-$toto = array(25689, 255555 ,2);
+$toto = array(25689, 2565 ,2);
 
