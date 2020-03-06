ff7a8f886a8c8f2e44498c74d1bcd5666fcb045c
jonard <gaetan.jonard@outlook.fr>
2020-03-06 10:07:46 +0100

    essay de config

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
-include_once 'model/groupesers.php';
-include_once 'controllers/configCtrl.php';
+
+//include_once 'controllers/configCtrl.php';
 $admin=array(255555);
-$authAdmin = array(255555, 2565);
-$toto = array(2, 255555, 2565);
+$authAdmin = array(25689, 2565);
+$toto = array(25689, 255555 ,2);
 
--- controllers/configCtrl.php
+++ controllers/configCtrl.php
@@ -1,1 +1,13 @@
 <?php
+include_once 'model/groupeusers.php';
+$listGroups = new groupeusers();
+$readGroups = $listGroups->groupeType();
+foreach ($readGroups AS $access){
+    $groupeID= $access->id;
+    $admin=$access->groupeType='Administrateur';
+    $idAdmin=$admin=$access->
+   $formateur=$access->groupeType='formateur';
+   if($access->groupeType==$formateur){
+       $userAccess=$groupeID;
+   }
+}
